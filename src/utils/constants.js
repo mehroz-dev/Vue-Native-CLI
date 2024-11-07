7c7bbc5cf1d45fda1d170fcbce03461d2939a416
Mehroz-Dev <mehrozzaheer3@gmail.com>
2024-11-07 19:39:46 +0500

    Complete Code

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+const constantObject = {
+  regExpForValidCrnaDirectory: /^[a-zA-Z0-9\-]+$/,
+  regExpForValidRnDirectory: /^[$A-Z_][0-9A-Z_$]*$/i,
+  crnaPackageName: "expo",
+  rnPackageName: "react-native",
+  stableRNVersion: "react-native@0.63",
+  appJsonPath: "app.json",
+  vueNativePackages: {
+    vueNativeCore: "vue-native-core",
+    vueNativeHelper: "vue-native-helper",
+    vueNativeScripts: "vue-native-scripts"
+  },
+  rnPkgCliFileName: "rn-cli.config.js",
+  metroConfigFile: "metro.config.js",
+  vueTransformerFileName: "vueTransformerPlugin.js",
+  appVueFileName: "App.vue",
+  expoAppJSONSourceExtsPath: "expo.packagerOpts.sourceExts",
+  vueFileExtensions: ["vue"],
+};
+
+module.exports = constantObject;
